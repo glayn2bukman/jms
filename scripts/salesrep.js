6e78f438ac047190912751657d8757d38328230f
glayn2bukman <glayn2bukman@gmail.com>
2017-07-18 16:06:05 +0300

    Add files via upload

--- scripts/salesrep.js
+++ scripts/salesrep.js
@@ -10,9 +10,4 @@ window.onload = function()
                 {
                     document.getElementById("known_client_label").onclick = check_known_client;
                     document.getElementById("new_client_label").onclick = check_new_client;
-
-                    var isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1; //&& ua.indexOf("mobile");
-                    if(isAndroid) {
-                        document.write('<meta name=\"viewport\" content=\"width=device-width,height='+window.innerHeight+', initial-scale=1.0\">');
-                    }
                 };
