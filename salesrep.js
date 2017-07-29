9adf7a6df8b6b1dfb86356cc4dea4cb6d48b1558
glayn2bukman <glayn2bukman@gmail.com>
2017-07-29 20:21:42 +0300

    Add files via upload

--- salesrep.js
+++ salesrep.js
@@ -526,6 +526,8 @@ window.onload = function()
     // set body size to a fixed value corresponding to the screen...
     document.getElementById("body").style.height = window.innerHeight+"px";
     document.getElementById("body").style.width = window.innerWidth+"px";
+    
+    alert(window.devicePixelRatio );
 
 };
                 
