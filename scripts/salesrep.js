fab359232015dbdbce197f7dd766e8e423f8cc96
glayn2bukman <glayn2bukman@gmail.com>
2017-07-29 19:19:44 +0300

    Add files via upload

--- scripts/salesrep.js
+++ scripts/salesrep.js
@@ -524,8 +524,10 @@ window.onload = function()
     start_connecting("fetching client segments...");
 
     // set body size to a fixed value corresponding to the screen...
-    document.getElementById("body").style.height = screen.height+"px";
-    document.getElementById("body").style.width = screen.width+"px";
+    //document.getElementById("body").style.height = screen.height+"px";
+    //document.getElementById("body").style.width = screen.width+"px";
+    document.body.style.height = screen.height+"px";    
+
 };
                 
                 
